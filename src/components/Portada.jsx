23d6522cb9f5ddf69795b1dbe009ccfda340c8d7
HerlanBenavides <94395211+HerlanBenavides@users.noreply.github.com>
2023-04-13 10:42:04 -0600

    up

--- src/components/Portada.jsx
+++ src/components/Portada.jsx
@@ -8,7 +8,7 @@ const Portada = () => {
             <div className="nombre"> <h2>Herlan Benavides</h2></div>
             <div className="diseno"><h4>Diseño Grafico</h4></div>
             <div className="empresa"><h5>Cosvic</h5></div>
-            <div className="imagen"><img src="src/assets/portada.png" alt="" /></div>
+            <div className="imagen"><img src={portada} alt="" /></div>
             <div className="frase"><h3>El diseño publicitario es una carrera que te permite dejar tu huella en el mundo, creando mensajes que pueden cambiar la forma en que las personas ven y experimentan el mundo que las rodea. </h3></div>
         </div>
     </div>
